5b3dd7801136250e4c389c4cb910893911ee92de
xluo12 <xluoforhire@gmail.com>
2019-04-03 09:25:05 -0400

    update

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
     users: [
@@ -34,7 +36,7 @@ app.get('/', (req, res) => {
 app.post('/signin', (req, res) => {
     if (req.body.email === database.users[0].email &&
         req.body.password === database.users[0].password) {
-        res.json('success')
+        res.json(database.users[0]);
     } else {
         res.status(400).json('error loggging in');
     }
@@ -51,7 +53,6 @@ app.post('/register', (req, res) => {
         id: '125',
         name: name,
         email: email,
-        password: password,
         entries: 0,
         joined: new Date(),
         }
