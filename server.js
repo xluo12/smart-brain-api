1bf0f9e79c0dc53aea1f29b0fd082f112938bc0a
xluo12 <xluoforhire@gmail.com>
2019-04-03 23:12:06 -0400

    use connectionString

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const image = require('./controllers/image');
 const db = require('knex')({
     client: 'pg',
     connection: {
-        host: process.env.DATABASE_URL,
+        connectionString: process.env.DATABASE_URL,
         ssl: true
     }
 }
