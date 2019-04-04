39a6e30390f8a3d477b81875aeb5458ce239efcd
xluo12 <xluoforhire@gmail.com>
2019-04-03 22:35:23 -0400

    delete old database

--- server.js
+++ server.js
@@ -21,28 +21,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            email: 'sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-    ]
-}
-
-app.get('/', (req, res) => res.send(database.users));
+app.get('/', (req, res) => res.send('It is working!'));
 app.post('/signin', (req, res) => signin.handleSignin(req, res, db, bcrypt));
 app.post('/register', (req, res) => register.handleRegister(req, res, db, bcrypt));
 app.get('/profile/:id', (req, res) => profile.handleProfileGet(req, res, db));
